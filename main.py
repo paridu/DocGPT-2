e590d6f30600fd2b85e551f01cc5714f8de6a3f8
Amirul <55971929+brightcosmo@users.noreply.github.com>
2024-07-16 13:20:56 +0000

    Hide system prompt

--- main.py
+++ main.py
@@ -9,8 +9,11 @@ def reset():
         "system",
         "content":
         """
-        You are a healthcare assistant. Based on the user's symptoms and conditions, provide a diagnosis if one is needed. Then recommend medications if they are applicable. If the patient states that they are at home or otherwise do not have access to medications, provide home remedies.
-        If a diagnosis is needed, explain the condition in simple language that a layman can understand.
+        You are a healthcare assistant. 
+        The user will ask you questions about their health.
+        Try to diagnose the patient with a specific condition, and explain the condition in simple language that a layman can understand.
+        If the symptoms given by the user are too vague, you can ask more questions to narrow down the diagnosis, unless the user just wants pain relief from their symptoms and not a proper diagnosis.
+        After a diagnosis is made, recommend medications if they are applicable. You can provide home remedies only if the medications are not typical in a household OR if the user specifies they can't access them at the current time.
         If the patient sounds worried, write an empathetic and supportive message to provide reassurance.
         Use the previous messages provided by the patient / yourself in order to give a more informed response.
         The user may provide you with images or videos of their injuries. If so, please describe what you see in the image, what is happening to them, the consequences, and what actions to take. Please be compassionate when doing this and try to soothe the patient's pain.
@@ -23,11 +26,21 @@ st.markdown("""
     div[data-testid="stVerticalBlock"] div:has(div.fixed-header) {
         position: sticky;
         top: 2rem;
-        background-color: #0e1117;
         z-index: 999;
         width: 100%;
     }
     .fixed-header {
+    }
+    div[data-testid="stVerticalBlock"] div:has(div.fixed-footer) {
+        position: sticky;
+        bottom: 7rem;
+        z-index: 999;
+        width: 100%;
+    }
+    .fixed-footer {
+        border-bottom: 0px solid black;
+        position: absolute;
+        bottom: 0px;
     }
 </style>
     """,
@@ -52,8 +65,9 @@ with header:
             reset()
 
 for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+    if message["role"] != "system":
+        with st.chat_message(message["role"]):
+                st.markdown(message["content"])
 
 if prompt := st.chat_input("Ask me anything!"):
     st.session_state.messages.append({"role": "user", "content": prompt})
@@ -74,3 +88,16 @@ if prompt := st.chat_input("Ask me anything!"):
         "role": "assistant",
         "content": response
     })
+
+
+footer = st.container()
+footer.write("""<div class='fixed-footer'/>""", unsafe_allow_html=True)
+
+with footer:
+    uploaded_file = st.file_uploader('', type=["jpg", "jpeg", "png"])
+    
+    if uploaded_file is not None:
+        st.session_state.messages.append({"role": "user", "content": "User uploaded an image"})
+        with st.chat_message("user"):
+            st.image(uploaded_file, use_column_width=True)
+st.markdown('</div>', unsafe_allow_html=True)
